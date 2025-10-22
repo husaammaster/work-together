cee45eea936fe596c775661aa4ee1dd7d29224cc
husam <husam.masalkhi@wegzwei.com>
2025-10-22 11:02:25 +0200

    working docker backend and couchDB

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ export const server = express();
 server.use(express.static('./public'));
 server.use(express.json());
 
-
+const port = process.env.PORT || 80;
 
 export const init = () => {
-    server.listen(80, err => {
+    server.listen(port, err => {
         if(err) console.log(err)
         else console.log('Server läuft');
     });
