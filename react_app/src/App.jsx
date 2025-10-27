a5b5e42b1e437649aadfa1bbf2f02085210c3b54
husam <husam.masalkhi@wegzwei.com>
2025-10-27 12:06:54 +0100

    functioning redux

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import {Route, Routes, Outlet, NavLink} from 'react-router-dom'
 import {ProjectListPage, MyProjectsPage, NotFoundPage} from './Pages'
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from './features/userSlice.js';
 
 const App = () => {
   return (
@@ -17,6 +19,13 @@ const App = () => {
 }
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user.name);
+
+  const handleUserChange = (evnt) => {
+    dispatch(setUser(evnt.target.value));
+  };
+
   return (
     <>
       <div className="backdrop-blur-2xl max-w-2xl mx-auto bg-blue-900/50 p-3 text-white sticky top-0 z-10 flex items-start justify-between">
@@ -25,8 +34,10 @@ const Header = () => {
           <p>Find projects you want to support</p>
         </div>
         <div className=" flex flex-col space-y-2">
-          <label htmlFor="user" className="text-sm font-medium">User</label>
-          <input id="user" type="text" placeholder="Name" className="text-sm border border-gray-600 rounded-md " />
+          <label htmlFor="user" className="text-sm font-medium">Eingeloggt als </label>
+          <input id="user" type="text" value={user} onChange={handleUserChange}
+            className="text-sm border border-gray-600 rounded-md "
+          />
         </div>
       </div>
       <Navbar />
@@ -35,12 +46,13 @@ const Header = () => {
 } 
 
 const Navbar = () => {
-  const user = 'martina';
+  const user = useSelector(state => state.user.name);
+
   return (
     <nav className="backdrop-blur-2xl max-w-2xl mx-auto bg-blue-900/50 p-3 text-white sticky top-21 z-10 flex items-start justify-between">
       <NavLink className="p-2 badge" to="/">Home</NavLink>
-      <NavLink className="p-2 badge" to="/my_projects/alex">Alex's Projekte</NavLink>
-      <NavLink className="p-2 badge" to={`/my_projects/${user}`}>Martina's Projekte</NavLink>
+      <NavLink className="p-2 badge" to="/my_projects/Alex">Alex's Projekte</NavLink>
+      <NavLink className="p-2 badge" to={`/my_projects/${user}`}>Meine eigenen Projekte</NavLink>
     </nav>
   )
 
@@ -48,7 +60,7 @@ const Navbar = () => {
 
 const Footer = () => {
   return (
-    <p>
+    <p className='text-center text-white dark:bg-gray-800 p-3 mb-0'>
       Copyright Helpers Inc. 2025
     </p>
   )
@@ -60,7 +72,7 @@ const Layout = () => {
     <>
       <Header />
       <Outlet />
-      <Footer />/
+      <Footer />
     </> 
   )
 }
